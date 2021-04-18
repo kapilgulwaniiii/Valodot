43771fd181b70fb7581bf72f6453eaaf0518fb07
kapil gulwani <[kapil_gulwani@persistent.com]>
2021-04-18 20:24:01 +0530

    Second minor update

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -3,9 +3,10 @@ import "./MainPage.css";
 function First() {
   return (
     <div id="grad1">
-      <h1 align="center">ValoDot</h1>
+      <h1 align="center">vAloDot</h1>
+      <h2 align="center"> VvIFS</h2>
       <h2 align="center"> 13-0</h2>
-      <img src={logo} class="center"></img>
+      <img src={logo} class="center" alt="Valorant_Image"/>
     </div>
   );
 }
