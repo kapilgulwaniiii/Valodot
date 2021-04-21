54a414c07d17918fcd45723c03115f957119c291
kapil gulwani <[kapil_gulwani@persistent.com]>
2021-04-22 01:16:39 +0530

    nav bar

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -3,12 +3,22 @@ import "./MainPage.css";
 function First() {
   return (
     <div id="grad1">
+      <div class="nav">
+        <a class="active" href="#home">
+          Home
+        </a>
+        <a href="#profile">Profiles</a>
+        <a href="#contact">Contact Us</a>
+        <a href="#settings">Settings</a>
+      </div>
       <h1 align="center">vAloDot</h1>
       <h2 align="center"> VvIFS</h2>
       <h2 align="center"> 13-0</h2>
       <img src={logo} class="center" alt="Valorant_Image" />
       <div class="ghost">
-      <a class="knowmore" href="#">Know More</a>
+        <a class="knowmore" href="#">
+          Know More
+        </a>
       </div>
       <div class="ghost">
         <a class="profiles" href="#">
